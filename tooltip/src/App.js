57cbee8388c7e17e92bd83c8a5e3fb385261dbf1
anjanpreet <anjanpreetsingh1019@gmail.com>
2023-03-21 01:16:24 +0530

    tooltip 1.0

---
+++ tooltip/src/App.js
@@ -0,0 +1,13 @@
+import Tooltip from "react-simple-tooltip";
+
+
+const App = () => (
+  <div style={{margin:"100px"}}>
+    <Tooltip content="😎">
+      <button>Hover me !</button>
+    </Tooltip>
+  </div>
+  
+)
+
+export default App;
